222cb040bf8bef6630f5245ea6298ff0f496ba6c
OSAMA263 <osamaelseify2@gmail.com>
2023-09-21 13:12:15 +0300

    y

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { terser } from "rollup-plugin-terser";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  input: "src/index.js",
-  output: {
-    file: "dist/bundle.js",
-    format: "cjs",
-  },
-  plugins: [react(), terser()],
-  build: {
-    rollupOptions: {
-      plugins: [terser()],
-    },
-  },
+  plugins: [react()],
   base: "/Portfolio-/",
 });
