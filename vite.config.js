e1ab265853d6a00d498e5d64fc9dd7d5c1f3d88e
OSAMA263 <osamaelseify2@gmail.com>
2023-09-21 12:31:38 +0300

    compress it?

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { terser } from "rollup-plugin-terser";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
-  plugins: [react()],base:"/Portfolio-/"
-})
+  plugins: [react(), terser()],
+  base: "/Portfolio-/",
+});
