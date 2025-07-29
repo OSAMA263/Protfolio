018eebbb8649f7c7793488a6984a76fd15d594c3
osama <osamaelseify2@gmail.com>
2025-07-30 02:02:48 +0300

    update cv

--- src/pages/Home/sections/WhoAmI.jsx
+++ src/pages/Home/sections/WhoAmI.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { motion } from "framer-motion";
 import tw from "tailwind-styled-components";
 import { AiOutlineDownload } from "react-icons/ai";
